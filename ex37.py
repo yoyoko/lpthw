644356dbd7967d5a02055585b417e8dab68aeadd
Edward Ahn <youngjin.ahn@gmail.com>
2017-10-14 23:25:21 -0400

    Ex 37 and Ex38

---
+++ ex37.py
@@ -0,0 +1,10 @@
+if True and True:
+    print("0")
+else:
+    print("1")
+
+with open("foo.txt") as file:
+    #do something
+    #No need to clean up, it will clean up by itself
+
+assert 2 + 2 == 5, "Houston, we've got a problem"
